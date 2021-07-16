5c41eb3dc974ea7043b03498aee571d47e35a009
Dan <kofsitho@twosun.com>
2021-07-16 19:03:01 +0900

    update

--- slider/index.js
+++ slider/index.js
@@ -11,10 +11,9 @@
       container: null,
       items: [],
       sliderIndex: 0,
-      delay: 500,
       speed: 500,
       autoPlay: false,
-      dot: true,
+      useDots: false,
     }
     this.options = Object.assign(defaultOpts, options);
     this.init();
@@ -23,6 +22,10 @@
   _slideShow.prototype = {
     init: function () {
       var { el, autoPlay } = this.options;
+
+      //
+      this.isAciveBtns = true;
+
 
       //calculate boxsize
       this.options.boxSize.width = parseInt(window.getComputedStyle(el).width) - 100;
@@ -36,7 +39,7 @@
       }
     },
     draw: function () {
-      var { el, boxSize, items, dot } = this.options;
+      var { el, boxSize, items, useDots } = this.options;
       el.classList.add("my-slider");
 
       var prevBtn = document.createElement("button");
@@ -81,18 +84,33 @@
       boxContainer.appendChild(itemElements[0].cloneNode(true));
       boxContainer.insertBefore(itemElements[itemElements.length - 1].cloneNode(true), itemElements[0]);
 
-
       this.currentPosition = -boxSize.width;
       boxContainer.style.transform = `translateX(${this.currentPosition}px)`;
 
-      if (dot) {
+      if (useDots) {
         this.drawDot();
       }
     },
     drawDot() {
+      var { el, items } = this.options;
+      var dotContainer = document.createElement("div");
+      dotContainer.className = "dot-container";
+      var dots = [];
+      for (let item in items) {
+        var dot = document.createElement("div");
+        dot.className = "dot";
+        dots.push(dot);
+        // dotContainer.appendChild(dot);
+      }
+      dots[0].classList.add("active");
+      dotContainer.append(...dots);
+      el.appendChild(dotContainer);
 
+      this.dots = dots;
     },
     setEventListener: function () {
+      var { useDots } = this.options;
+
       const self = this;
       this.prevBtn.addEventListener("click", function (e) {
         // console.log(e);
@@ -105,28 +123,46 @@
       });
 
       this.options.container.addEventListener('transitionend', this.checkIndex.bind(this));
+
+      if (useDots) {
+        for (let dot of this.dots) {
+          dot.addEventListener("click", function (e) {
+            const target = e.target;
+            const index = this.dots.indexOf(target);
+            if (this.options.sliderIndex != index) {
+              this.moveTo(index);
+            }
+          }.bind(this))
+        }
+      }
     },
     play: function () {
-      var { container, boxSize, sliderIndex, items, delay, speed } = this.options;
+      var { container, boxSize, sliderIndex, items, speed } = this.options;
+    },
+    moveTo: function (toIdx) {
+      // if (!this.isAciveBtns) {
+      //   return;
+      // }
+      // this.isAciveBtns = false;
 
-      var timer = setInterval(() => {
-        console.log(sliderIndex);
-        if (sliderIndex == items.length - 1) {
-          clearInterval(timer);
-          return;
-        }
-        ++sliderIndex;
-        const currentPos = sliderIndex * boxSize.width;
-        container.style.transform = `translateX(-${currentPos}px)`;
-      }, speed + delay)
+      var { container, boxSize, sliderIndex, items, speed } = this.options;
+      container.style['transition'] = `transform ease ${speed}ms`;
+
+      const movePos = -((toIdx + 1) * boxSize.width);
+      // console.log(movePos, this.currentPosition);
+      container.style.transform = `translateX(${movePos}px)`;
+      this.options.sliderIndex = toIdx;
+      this.currentPosition = movePos;
     },
     moveLeft: function () {
-      var { container, boxSize, sliderIndex, items, delay, speed } = this.options;
+      if (!this.isAciveBtns) {
+        return;
+      }
+      this.isAciveBtns = false;
+      var { container, boxSize, sliderIndex, items, speed } = this.options;
       console.log("moveLeft");
 
       container.style['transition'] = `transform ease ${speed}ms`;
-
-      // const movePos = -(this.options.sliderIndex * boxSize.width);
 
       const movePos = this.currentPosition + boxSize.width;
       container.style.transform = `translateX(${movePos}px)`;
@@ -135,7 +171,11 @@
       this.currentPosition = movePos;
     },
     moveRight: function () {
-      var { container, boxSize, sliderIndex, items, delay, speed } = this.options;
+      if (!this.isAciveBtns) {
+        return;
+      }
+      this.isAciveBtns = false;
+      var { container, boxSize, sliderIndex, items, speed } = this.options;
       console.log("moveRight");
 
       container.style['transition'] = `transform ease ${speed}ms`;
@@ -149,7 +189,7 @@
 
     checkIndex: function () {
       console.log("checkIndex");
-      var { container, sliderIndex, items, boxSize } = this.options;
+      var { container, sliderIndex, items, boxSize, useDots } = this.options;
 
       container.style.removeProperty('transition');
       if (sliderIndex == -1) {
@@ -163,6 +203,17 @@
         this.options.sliderIndex = 0;
         this.currentPosition = movePos;
       }
+
+
+      if (useDots) {
+        for (let dot of this.dots) {
+          dot.classList.remove("active");
+        }
+        console.log(this.options.sliderIndex);
+        this.dots[this.options.sliderIndex].classList.add("active");
+      }
+
+      this.isAciveBtns = true;
     }
   }
 
